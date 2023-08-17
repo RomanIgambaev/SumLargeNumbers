b421b6f83da06bdd9dc92c12841d0a2407b1188f
RomanIgambaev <roman.igambaev11@gmail.com>
2023-08-17 16:40:46 +0500

    Large numbers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Введите второе число: ");
+        double num2 = scanner.nextDouble();
+        System.out.print("Введите третье число: ");
+        double num3 = scanner.nextDouble();
+        double max1, max2;
+        if (num1 >= num2 && num1 >= num3) {
+            max1 = num1;
+            if (num2 >= num3) {
+                max2 = num2;
+            } else {
+                max2 = num3;
+            }
+        } else if (num2 >= num1 && num2 >= num3) {
+            max1 = num2;
+            if (num1 >= num3) {
+                max2 = num1;
+            } else {
+                max2 = num3;
+            }
+        } else {
+            max1 = num3;
+            if (num1 >= num2) {
+                max2 = num1;
+            } else {
+                max2 = num2;
+            }
+        }
+        double sumOfMax = max1 + max2;
+        System.out.println("Сумма двух наибольших чисел: " + sumOfMax);
     }
 }
+
